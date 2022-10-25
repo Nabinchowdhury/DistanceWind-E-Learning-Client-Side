533e46650360818228b2bde999aeca1386243ccc
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-10-25 06:11:28 +0600

    home page done

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { FaArrowRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import { ThemeContext } from '../layout/Main/Main';
 import "./Home.css"
 const Home = () => {
@@ -9,7 +10,7 @@ const Home = () => {
             <div className={`mx-auto text-center w-full md:w-1/2 absolute top-1/3
            ${ETheme ? "right-0 md:right-10" : "left-0 md:left-10"}`}>
                 <p className='text:xl md:text-3xl'>Learn as if you were not reaching your goal and as though you were scared of missing it. Want to view our courses?</p>
-                <button className={`btn ${ETheme ? "btn-warning" : "btn-primary"} mt-5 px-5 font-bold`}>Get Started <FaArrowRight className='ml-3'></FaArrowRight></button>
+                <Link to="/courses"><button className={`btn ${ETheme ? "btn-warning" : "btn-primary"} mt-5 px-5 font-bold`}>Get Started <FaArrowRight className='ml-3'></FaArrowRight></button></Link>
             </div>
         </div >
     );
