f141cd7652c742590680269dddfc8c6ad1a58bfb
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-10-26 02:04:20 +0600

    logout done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,11 @@ import { AuthContext } from '../Contexts/AuthProvider/AuthProvider';
 
 const Header = () => {
     const { ETheme, setETheme } = useContext(ThemeContext)
-    const { user } = useContext(AuthContext)
+    const { user, logUserOut } = useContext(AuthContext)
+    const handleLogOut = () => {
+        logUserOut()
+    }
+
     return (
         <div>
             <div className="navbar  lg:px-10">
@@ -35,7 +39,7 @@ const Header = () => {
                         <li className='px-1' ><Link to="/FAQ">FAQ </Link></li>
                         <li className='px-1'><Link to="/blog">Blog</Link></li>
                         <li className='px-1'><Link onClick={() => setETheme(!ETheme)}>Theme {ETheme ? <FaSun></FaSun> : <FaMoon></FaMoon>}</Link></li>
-                        {user ? <button className='btn btn-outline btn-error mx-2'>Log out</button> : <Link to="/login"><button className='btn btn-outline btn-warning mx-2'>Log in</button></Link>}
+                        {user ? <button className='btn btn-outline btn-error mx-2' onClick={handleLogOut}>Log out</button> : <Link to="/login"><button className='btn btn-outline btn-warning mx-2'>Log in</button></Link>}
                     </ul>
                 </div>
 
