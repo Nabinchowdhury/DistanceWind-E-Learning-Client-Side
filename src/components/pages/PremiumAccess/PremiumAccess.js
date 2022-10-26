cc2180a51592904f52a0cb84b63cce13ba20b3fd
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-10-26 07:28:44 +0600

    updated

--- src/components/pages/PremiumAccess/PremiumAccess.js
+++ src/components/pages/PremiumAccess/PremiumAccess.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { useEffect } from 'react';
-import { FaCoins } from 'react-icons/fa';
+import { FaArrowLeft, FaCoins, FaDollarSign } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
 import LeftNav from '../Courses/LeftNav/LeftNav';
 import { ThemeContext } from '../layout/Main/Main';
@@ -13,6 +13,7 @@ const PremiumAccess = () => {
     const { ETheme } = useContext(ThemeContext)
     const [coursNames, setCourseNames] = useState([])
     const [Lcoins, setLcoins] = useState(0)
+    const [coupon, setCoupon] = useState(false)
     useEffect(() => {
         fetch(`https://b610-learning-platform-server-nabinchowdhury.vercel.app/courses`)
             .then(res => res.json())
@@ -38,21 +39,27 @@ const PremiumAccess = () => {
             </div>
             <div className={`lg:col-span-4 xl:col-span-3`}>
 
-                <div >
-                    <div>
+                <div className='h-screen'>
+                    <div className={coupon ? "block" : "hidden"}>
                         <h1>there willl be something</h1>
-
+                        <div className="mockup-window border border-base-300">
+                            <div className="flex justify-center px-4 py-16 border-t border-base-300">Hello!</div>
+                        </div>
                     </div>
 
 
 
-                    <div className="card lg:card-side bg-transparent shadow-xl">
+                    <div className="card xl:card-side bg-transparent shadow-xl mt-7">
                         <figure><img className='h-full w-full' src={image} alt="Album" /></figure>
                         <div className="card-body">
-                            <h2 className="card-title">Course : {title}</h2>
-                            <p>Click the button to listen on Spotiwhy app.</p>
+                            <h2 className="card-title font-bold">Course : {title}</h2>
+                            <div className='flex justify-center items-center'><p className='flex justify-left items-center text-xl'>Price: {price}  <FaDollarSign ></FaDollarSign> / {coins} <FaCoins className='ml-2'></FaCoins> </p>
+                                <button className='btn btn-outline btn-primary font-bold'><FaArrowLeft className='mr-2'></FaArrowLeft> Get Course</button>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni possimus laudantium, quod quae praesentium error porro maxime numquam laborum exercitationem dolor accusamus voluptates delectus, perspiciatis, soluta eius sunt omnis voluptatum.</p>
+
                             <div className="card-actions justify-center">
-                                <div className="stats stats-vertical sm:stats-horizontal shadow">
+                                <div className="stats w-full stats-vertical sm:stats-horizontal shadow">
 
                                     <div className="stat place-items-center">
                                         <div className="stat-title">Duration</div>
@@ -68,7 +75,7 @@ const PremiumAccess = () => {
 
                                     <div className="stat place-items-center">
                                         <div className="stat-title">Achieve on complete</div>
-                                        <div className="stat-value">{coins / 10} <FaCoins></FaCoins></div>
+                                        <div className="stat-value flex justify-left items-center">{coins / 10} <FaCoins className='ml-2'></FaCoins></div>
                                         <div className="stat-desc">↘︎ 90 (14%)</div>
                                     </div>
 
