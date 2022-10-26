31b77ccc2456126a7af2b3e8df43c850cf113890
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-10-26 06:47:14 +0600

    premium page style updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,9 +14,9 @@ const Header = () => {
     }
 
     return (
-        <div>
-            <div className="navbar  lg:px-10">
-                <div className="navbar-start  w-full">
+        <div >
+            <div className="navbar lg:px-10 ">
+                <div className="navbar-start  w-full ">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -37,7 +37,7 @@ const Header = () => {
                                 <Link to="/login" className='w-1/2'><button className='btn btn-outline btn-warning btn-sm  my-2'>Log in</button></Link>}
                         </ul>
                     </div>
-                    <div className='flex items-center'>
+                    <div className='flex items-center '>
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGVN7KTJO2fVxTtArncxZdkGPld5-EZlCFTHPtNwwkeG2Q3t9a8mrOQqGJzPVnHlFnnYc&usqp=CAU" alt="" className='rounded w-1/6 rounded-full' />
                         <p className='text-center'> <Link to="/" className="ml-3 font-serif font-extrabold text-lg sm:text-2xl md:text-3xl lg:text-4xl ">DistanceWind E-Learning</Link></p>
                     </div>
--- src/components/pages/PremiumAccess/PremiumAccess.js
+++ src/components/pages/PremiumAccess/PremiumAccess.js
@@ -1,14 +1,18 @@
 import React, { useContext, useState } from 'react';
 import { useEffect } from 'react';
+import { FaCoins } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
 import LeftNav from '../Courses/LeftNav/LeftNav';
 import { ThemeContext } from '../layout/Main/Main';
 
 const PremiumAccess = () => {
     const premiumCourse = useLoaderData()
+    console.log(premiumCourse)
+    const { id, title, image, Duration, lessons, price, coins } = premiumCourse
+
     const { ETheme } = useContext(ThemeContext)
     const [coursNames, setCourseNames] = useState([])
-    const [coins, setCoins] = useState(0)
+    const [Lcoins, setLcoins] = useState(0)
     useEffect(() => {
         fetch(`https://b610-learning-platform-server-nabinchowdhury.vercel.app/courses`)
             .then(res => res.json())
@@ -17,14 +21,13 @@ const PremiumAccess = () => {
     return (
         <div className='flex flex-col lg:grid lg:grid-cols-6 xl:grid-cols-4 gap-4 ' >
             <div className='lg:col-span-2 xl:col-span-1 '>
-                <div className={`mt-20 sticky top-20 h-auto`}>
+                <div className={`mt-6 sticky top-20 h-auto`}>
                     <div className={`${ETheme ? 'themeLight' : "themeDark"}  rounded-2xl `}>
 
                         <div className="card  bg-black opacity-70  shadow-xl">
                             <div className="card-body ">
-                                <h2 className="card-title mx-auto text-white border">Offers and Achievements</h2>
-                                <p className='text-white'>You Have {coins} coins</p>
-
+                                <h2 className="card-title mx-auto text-white ">Offers and Achievements</h2>
+                                <p className='text-white'>You Have {Lcoins} coins</p>
                             </div>
                         </div>
                         {
@@ -35,10 +38,45 @@ const PremiumAccess = () => {
             </div>
             <div className={`lg:col-span-4 xl:col-span-3`}>
 
+                <div>
+                    <div>
+                        <h1>there willl be something</h1>
 
-                {
+                    </div>
 
-                }
+
+
+                    <div className="card lg:card-side bg-base-100 shadow-xl">
+                        <figure><img src={image} alt="Album" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">Course : {title}</h2>
+                            <p>Click the button to listen on Spotiwhy app.</p>
+                            <div className="card-actions justify-center">
+                                <div className="stats stats-vertical sm:stats-horizontal shadow">
+
+                                    <div className="stat place-items-center">
+                                        <div className="stat-title">Duration</div>
+                                        <div className="stat-value">{Duration}/day</div>
+                                        <div className="stat-desc">From January 1st to February 1st</div>
+                                    </div>
+
+                                    <div className="stat place-items-center">
+                                        <div className="stat-title">Lessons</div>
+                                        <div className="stat-value text-secondary">{lessons}/week</div>
+                                        <div className="stat-desc text-secondary">↗︎ 40 (2%)</div>
+                                    </div>
+
+                                    <div className="stat place-items-center">
+                                        <div className="stat-title">Achieve on complete</div>
+                                        <div className="stat-value">{coins / 10} <FaCoins></FaCoins></div>
+                                        <div className="stat-desc">↘︎ 90 (14%)</div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
