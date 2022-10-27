f86fd036909804804ea3cb139a1219c2e8473f7a
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-10-27 17:19:53 +0600

    course card price  updated

--- src/components/pages/Courses/Courses/CourseCard/CourseCard.js
+++ src/components/pages/Courses/Courses/CourseCard/CourseCard.js
@@ -7,7 +7,7 @@ import { ThemeContext } from '../../../layout/Main/Main';
 const CourseCard = ({ course }) => {
     // console.log(course)
     const { ETheme } = useContext(ThemeContext)
-    const { Duration, coins, id, image, lessons, price, title } = course
+    const { coins, id, image, price, title } = course
     return (
         <div>
             <div className={`card ${ETheme ? "bg-slate-200" : "bg-gray-700"} w-11/12 h-full shadow-xl `}>
@@ -17,6 +17,7 @@ const CourseCard = ({ course }) => {
                         {title}
                         <div className="badge badge-secondary">{coins} <FaCoins className='ml-2'></FaCoins></div>
                     </h2>
+                    <p className='flex items-end'>Price: {price} <FaDollarSign className=' my-1'></FaDollarSign></p>
                     <div className="card-actions  mt-5">
                         <Link to={`/course/${id}`}><button className='btn btn-warning'>See Details <FaArrowRight className='ml-2'></FaArrowRight></button></Link>
                     </div>
